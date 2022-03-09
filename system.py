950fd3fd2a960ea104e2c1f8ba25ef2bd2563c5b
Hridyansh <dugar@ualberta.ca>
2022-03-08 18:39:38 -0700

    part 2

--- system.py
+++ system.py
@@ -27,6 +27,10 @@ class Session:
     def end_session(self):
         self.end_time = time.time()
         self.duration = (self.end_time - self.start_time)//60.0
+        query = "UPDATE sessions set duration = :dur where sid = :sid"
+        self.log.cursor.execute(query,{'dur':self.duration,'sid':self.session_id})
+        self.log.conn.commit()
+
 
 
 
@@ -38,8 +42,14 @@ class System:
     def start_session(self):
         self.session = Session(self.log)
 
+    def end_session(self):
+        # TODO: end movies and add to session duration
+        self.session.end_session()
+
 if __name__ == '__main__':
     l1 = Login("c100","cmput291","./mini-proj.db")
     l1.login()
     s1 = System(l1)
     s1.start_session()
+    time.sleep(120)
+    s1.end_session()
